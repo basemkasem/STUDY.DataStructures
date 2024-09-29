6c536bc69df799714f803483ca2dfea248229e8c
basemkasem <basem.kasem16@gmail.com>
2024-09-29 15:37:47 +0300

    Create "EnhancedArray" class. Create resize method in "EnhancedArray" class.

---
+++ STUDY.DataStructures/EnhancedArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.DataStructures;
+
+public class EnhancedArray
+{
+    public void Resize<T>(ref T[] source, int newSize)
+    {
+        if (newSize <= 0 || source == null) return;
+        T[] newArray = new T[newSize];
+        Buffer.BlockCopy(source,0, newArray, 0, Buffer.ByteLength(source));
+        source = newArray;
+    }
+
+    public static void PrintArray<T>(T[] array) 
+    {
+        Console.WriteLine("{" + String.Join(", ", array) + "}");
+    }
+}
--- STUDY.DataStructures/Program.cs
+++ STUDY.DataStructures/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using STUDY.DataStructures;
+
+int[] array = { 1, 2, 3 };
+Console.WriteLine(array.Length);
+EnhancedArray.PrintArray(array);
+
+EnhancedArray newResizedArray = new EnhancedArray();
+newResizedArray.Resize<int>(ref array, 7);
+
+Console.WriteLine(array.Length);
+EnhancedArray.PrintArray(array);
